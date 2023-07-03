d3f1aec5ea4597a90f8b7297ef6101b76fd140e4
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-07-03 23:38:38 +0800

    searchbartabs for patient record and search screen

--- capstone_sams/lib/constants/Dimensions.dart
+++ capstone_sams/lib/constants/Dimensions.dart
@@ -1,3 +1,6 @@
+ 
 const mobileWidth = 480;
 const tabletWidth = 800;
 const desktopWidth = 1440;
+
+ 
--- capstone_sams/lib/screens/patient-record/PatientRecordScreen.dart
+++ capstone_sams/lib/screens/patient-record/PatientRecordScreen.dart
@@ -1,3 +1,4 @@
+import 'package:capstone_sams/global-widgets/Dashboard.dart';
 import 'package:capstone_sams/global-widgets/SearchBarTabs.dart';
 import 'package:flutter/material.dart';
 import '../../global-widgets/SearchBarTabs.dart';
@@ -8,9 +9,14 @@ class PatientRecordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      endDrawer: Dashboard(
+          profile:
+              'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'),
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight),
-        child: SearchBarTabs(),),
+        preferredSize: Size.fromHeight(100),
+        child: SearchBarTabs(),
+        
+      ),
     );
   }
 }
