5678c2897d7b65620a677c94cf9eb0f6d1438be2
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-07-07 11:09:43 +0800

    can now upload file in webapp

--- capstone_sams/lib/sams_server/api/models.py
+++ capstone_sams/lib/sams_server/api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import  AbstractBaseUser, BaseUserManager, PermissionsMixin 
+import os
+import datetime
+from django import forms
 # Create your models here.
 class AccountManager(BaseUserManager):
     def create_user(self, username, password=None, **extra_fields):
@@ -47,3 +50,40 @@ class Account(AbstractBaseUser, PermissionsMixin):
     @property
     def is_authenticated(self):
         return True
+    
+
+
+# def filepath(request, filename):
+#     old_filename  =filename
+#     timeNow = datetime.datetime.now().strftime('%Y%m%d%H:%M:%S')
+#     filename = '%%' % (timeNow, old_filename)
+#     return os.path.join('uploads/', filename)
+
+# class LabResult(models.Model):
+#     name = models.TextField(max_length=128)
+#     comment = models.TextField(max_length=512)
+#     pdf = models.
+
+# class UploadFileForm(forms.Form):
+#     name = forms.CharField(max_length=50)
+#     comments = forms.TimeField()
+#     file = forms.FileField(upload_to=filepath, null=True, blank=True)
+
+class LabResult(models.Model):
+    title = models.CharField(max_length=128)
+    comment = models.TextField(max_length=512)
+    pdf = models.FileField(upload_to='upload/')
+ 
+    class Meta:
+        ordering = ['title']
+     
+    def __str__(self):
+        return f"{self.title}"
+
+class UploadLabResult(forms.ModelForm):
+    class Meta:
+        model = LabResult
+        fields = ('title', 'comment','pdf')
+
+class pdfJson(models.Model):
+    text = models.TextField()
--- capstone_sams/lib/sams_server/sams_server/urls.py
+++ capstone_sams/lib/sams_server/sams_server/urls.py
@@ -3,12 +3,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.contrib import admin
-from django.urls import path
-from api.views import process_pdf
+from django.urls import path, include
+from api.views import process_pdf, ViewLabResult
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/ocr/', process_pdf, name='process_pdf'),
+    # path('api/ocr/', process_pdf, name='process_pdf'),
+    path('api/ocr/', ViewLabResult, name='ViewLabResult'),
 ]
