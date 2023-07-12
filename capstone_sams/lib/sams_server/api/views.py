402bb0c6b513d122786a915e1785d5dcbfecceb3
justingabi <113955257+justingabi@users.noreply.github.com>
2023-07-12 16:09:29 +0800

    made url changes

--- capstone_sams/lib/sams_server/api/views.py
+++ capstone_sams/lib/sams_server/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 
 from django.shortcuts import render, redirect
 from api.serializers import AccountSerializer, PatientSerializer, MedicineSerializer, HealthRecordSerializer, SymptomSerializer, CommentSerializer, PrescriptionSerializer, MedicineSerializer, PersonalNoteSerializer
-from tabula.io import read_pdf
+# from tabula.io import read_pdf
 from django.contrib import messages
 from django.http import JsonResponse
 
@@ -15,7 +15,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from disease_prediction.prediction_model import predict_disease
+# from disease_prediction.prediction_model import predict_disease
 # def process_pdf(request):
 #     # file_path = "\Users\nulltest\ocr\cbc.pdf"
 #     df = read_pdf(file_path, pages='all', encoding='cp1252')
@@ -50,24 +50,24 @@ from disease_prediction.prediction_model import predict_disease
 #     return render(request, 'upload.html', {'form': form})
 
 
-def ViewLabResult(request):
-    if request.method == 'POST':
-        form = UploadLabResult(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('The file is saved')
-    else:
-        form = UploadLabResult()
-    return render(request, 'ocr.html', {'form': form})
+# def ViewLabResult(request):
+#     if request.method == 'POST':
+#         form = UploadLabResult(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('The file is saved')
+#     else:
+#         form = UploadLabResult()
+#     return render(request, 'ocr.html', {'form': form})
 
 
-def process_pdf(request):
-    pdf_path = "cbc.pdf"
-    dfs = read_pdf(pdf_path, pages='all', encoding='cp1252',
-                   pandas_options={'header': True})
-    json_data = dfs[1].to_json(orient='columns')
-    print(json_data)
-    dfs[0].to_json('output.json', orient='columns')
+# def process_pdf(request):
+#     pdf_path = "cbc.pdf"
+#     dfs = read_pdf(pdf_path, pages='all', encoding='cp1252',
+#                    pandas_options={'header': True})
+#     json_data = dfs[1].to_json(orient='columns')
+#     print(json_data)
+#     dfs[0].to_json('output.json', orient='columns')
 
 
 class PatientView(viewsets.ModelViewSet):
@@ -86,6 +86,7 @@ class PatientView(viewsets.ModelViewSet):
                 phone=patient_data['phone'],
                 email=patient_data['email']
             )
+            patient.save()
             return Response({"message": "Patient created successfully."}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({"message": "Failed to create patient.", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
@@ -95,16 +96,16 @@ class PatientView(viewsets.ModelViewSet):
         try:
             queryset = Patient.objects.all()
             serializer = PatientSerializer(queryset, many=True)
-            return Response({"message": "Patients fetched successfully."}, serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message": "Failed to fetch patients.", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     @api_view(['GET'])
     def fetch_patient_by_id(request, patientID):
         try:
-            queryset = Patient.objects.get(pk=patientID)
-            serializer = PatientSerializer(queryset)
-            return Response({"message": "Patient fetched successfully."}, serializer.data, status=status.HTTP_200_OK)
+            queryset = Patient.objects.filter(pk=patientID)
+            serializer = PatientSerializer(queryset.first())
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message": "Failed to fetch patients.", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -136,66 +137,69 @@ class MedicineView(viewsets.ModelViewSet):
         try:
             queryset = Medicine.objects.all()
             serializer = MedicineSerializer(queryset, many=True)
-            return Response({"message": "Medicines fetched successfully."}, serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message": "Failed to fetch medicines.", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
         
     @api_view(['GET'])
     def fetch_medicine_by_id(request, drugID):
         try:
-            queryset = Medicine.objects.get(pk=drugID)
+            queryset = Medicine.objects.filter(pk=drugID)
             serializer = MedicineSerializer(queryset, many=True)
-            return Response({"message": "Medicine fetched successfully."}, serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({"message": "Failed to fetch medicne.", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Failed to fetch medicine.", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
-class HealthRecordView(viewsets.ModelViewSet):
+class HealthRecordView(viewsets.ViewSet):
 
     @api_view(['GET'])
     def fetch_records(request):
         try:
             records = Health_Record.objects.all()
+            response_data = []
             for record in records:
-                history = HealthRecordSerializer(record).data
-                patient_symptoms = Patient_Symptom.objects.filter(
-                medical_history__in=history)
-                symptoms = Symptom.objects.filter(
-                    patient_symptoms__in=patient_symptoms)
+                history = Medical_History.objects.filter(histNum__in=record)
+                patient = Patient.objects.filter(patientID__in=history)
+                patient_symptoms = Patient_Symptom.objects.filter(medical_history__in=history)
+                symptoms = Symptom.objects.filter(patient_symptoms__in=patient_symptoms)
                 comments = Comment.objects.filter(health_record=record)
                 physician = Account.objects.filter(comment__in=comments)
-                prescription = Prescription.objects.filter(
-                    health_record=record)
-                prescribed_medicines = Prescribed_Medicine.objects.filter(
-                prescription__in=prescription)
-                medicines = Medicine.objects.filter(
-                    prescribed_medicine__in=prescribed_medicines)
+                prescription = Prescription.objects.filter(health_record=record)
+                prescribed_medicines = Prescribed_Medicine.objects.filter(prescription__in=prescription)
+                medicines = Medicine.objects.filter(prescribed_medicine__in=prescribed_medicines)
+                # Serialize the data
+                history_data = HealthRecordSerializer(record).data
+                patient_data = PatientSerializer(patient, many=True).data
+                symptoms_data = SymptomSerializer(symptoms, many=True).data
+                comments_data = CommentSerializer(comments, many=True).data
+                physician_data = AccountSerializer(physician, many=True).data
+                prescription_data = PrescriptionSerializer(prescription, many=True).data
+                medicines_data = MedicineSerializer(medicines, many=True).data
 
-            # Serialize the data
-            history_data = HealthRecordSerializer(record).data
-            symptoms_data = SymptomSerializer(symptoms, many=True)
-            comments_data = CommentSerializer(comments, many=True)
-            physician_data = AccountSerializer(physician, many=True)
-            prescription_data = PrescriptionSerializer(prescription, many=True)
-            medicines_data = MedicineSerializer(medicines, many=True)
+                record_data = {
+                    'history': history_data,
+                    'physicians': physician_data,
+                    'patients': patient_data,
+                    'symptoms': symptoms_data,
+                    'comments': comments_data,
+                    'prescription': prescription_data,
+                    'medicines': medicines_data,
+                }
 
-            response_data = {
-                'history': history_data.data,
-                'physicians': physician_data.data,
-                'symptoms': symptoms_data.data,
-                'comments': comments_data.data,
-                'presription': prescription_data.data,
-                'medicines': medicines_data.data,
-            }
+                response_data.append(record_data)
 
-            return Response({"message": "Records fetched successfully."}, response_data, status=status.HTTP_200_OK)
+            return Response(response_data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message": "Failed to fetch health records", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
+
     @api_view(['GET'])
-    def fetch_record_by_num(request, recordNum):
+    def fetch_record_by_num(request):
         try:
+            data = json.loads(request.body)
+            recordNum = data['recordNum']
             record = Health_Record.objects.get(pk=recordNum)
             history = Medical_History.objects.filter(health_record=record)
             patient_symptoms = Patient_Symptom.objects.filter(
@@ -226,7 +230,7 @@ class HealthRecordView(viewsets.ModelViewSet):
                 'medicines': medicines_data.data,
             }
 
-            return Response({"message": "Record fetched successfully."}, response_data, status=status.HTTP_200_OK)
+            return Response(response_data, status=status.HTTP_200_OK)
 
         except Exception as e:
             return Response({"message": "Failed to fetch health record", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
@@ -238,7 +242,7 @@ class SymptomsView(viewsets.ModelViewSet):
         try:
             symptoms = Symptom.objects.all()
             serializer = SymptomSerializer(symptoms, many=True)
-            return Response(serializer.dart, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message": "Failed to fetch symptoms", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
     
@@ -247,7 +251,7 @@ class SymptomsView(viewsets.ModelViewSet):
         try:
             symptoms = Symptom.objects.get(pk=sympNum)
             serializer = SymptomSerializer(symptoms)
-            return Response(serializer.dart, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message": "Failed to fetch symptoms", "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -258,7 +262,7 @@ class PersonalNotesView(viewsets.ModelViewSet):
         try:
             personal_notes = Personal_Note.objects.filter(account__accountID=accountID)
             serializer = PersonalNoteSerializer(personal_notes, many=True)
-            return Response({"message": "Notes fetched successfully"}, serializer.data, status=status.HTTP_200_OKs)
+            return Response(serializer.data, status=status.HTTP_200_OKs)
         except Exception as e:
             return Response({"message": "Failed to fetch personal notes"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -271,6 +275,7 @@ class PersonalNotesView(viewsets.ModelViewSet):
                 content = notes_data['content'],
                 account = notes_data['account']
             )
+            note.save()
             return Response({"message": "Note successfully created"}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({"message": "Failed to create note"}, status=status.HTTP_400_BAD_REQUEST)
--- capstone_sams/lib/sams_server/sams_server/urls.py
+++ capstone_sams/lib/sams_server/sams_server/urls.py
@@ -1,30 +1,30 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 from django.contrib import admin
 from django.urls import path, include
-from api.views import process_pdf, ViewLabResult, PatientView, MedicineView, HealthRecordView, SymptomsView, PersonalNotesView, predict
-
+from api.views import  PatientView, MedicineView, HealthRecordView, SymptomsView, PersonalNotesView
+# process_pdf, ViewLabResult, predict
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('api/ocr/', process_pdf, name='process_pdf'),
-    path('api/ocr/', ViewLabResult, name='ViewLabResult'),
+    # path('api/ocr/', ViewLabResult, name='ViewLabResult'),
     path('api/patients/create/', PatientView.create_patient, name='create_patient'),
-    path('api/patients/get/', PatientView.fetch_patients, name='fetch_patients'),
-    path('api/patients/get/<str:patientID>', PatientView.fetch_patient_by_id, name='fetch_patient'),
+    path('api/patients/', PatientView.fetch_patients, name='fetch_patients'),
+    path('api/patients/<str:patientID>', PatientView.fetch_patient_by_id, name='fetch_patient'),
     path('api/patients/update/', PatientView.update_patient, name='update_patients'),
-    path('api/medicines/get/', MedicineView.fetch_medicine, name='fetch_medicines'),
-    path('api/medicines/get/<str:drugID>', MedicineView.fetch_medicine_by_id, name='fetch_medicine'),
-    path('api/records/get/', HealthRecordView.fetch_records, name='fetch_records'),
-    path('api/records/get/<str:recordNum>', HealthRecordView.fetch_record_by_num, name='fetch_record'),
-    path('api/symptoms/get/', SymptomsView.fetch_symptoms, name='fetch_symptoms'),
-    path('api/symptoms/get/<str:sympNum>', SymptomsView.fetch_symptoms_by_num, name='fetch_symptom'),
+    path('api/medicines/', MedicineView.fetch_medicine, name='fetch_medicines'),
+    path('api/medicines/<str:drugID>', MedicineView.fetch_medicine_by_id, name='fetch_medicine'),
+    path('api/records/', HealthRecordView.fetch_records, name='fetch_records'),
+    path('api/records/<str:recordNum>', HealthRecordView.fetch_record_by_num, name='fetch_record'),
+    path('api/symptoms/', SymptomsView.fetch_symptoms, name='fetch_symptoms'),
+    path('api/symptoms/<str:sympNum>', SymptomsView.fetch_symptoms_by_num, name='fetch_symptom'),
     path('api/notes/get/<str:accountID>', PersonalNotesView.fetch_personal_notes, name='fetch_personal_notes'),
     path('api/notes/create/', PersonalNotesView.create_personal_note, name='create_personal_note'),
     path('api/notes/update/<int:noteNum>', PersonalNotesView.update_personal_note, name='update_personal_note'),
     path('api/notes/delete/<int:noteNum>', PersonalNotesView.delete_personal_note, name='delete_personal_note'),
-    path('predict/', predict),
+    # path('predict/', predict),
 ]
