d3318d978f59aba67d51f84e5f5a8d9aef27010c
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-07-16 16:07:01 +0800

    few fixes on url paths

--- capstone_sams/lib/sams_server/api/modules/cpoe/urls.py
+++ capstone_sams/lib/sams_server/api/modules/cpoe/urls.py
@@ -2,12 +2,12 @@ from .views import CommentView, MedicineView
 from django.urls import path
 
 urlpatterns = [
-    path('api/comments/create/<str:accountId>/<str:recordNum>/', CommentView.create_comment, name='create_comment'),
-    path('api/comments/get/', CommentView.fetch_comments, name='fetch_comments'),
-    path('api/comments/get/<str:accountId>', CommentView.fetch_comments_by_account_id, name='fetch_comments_by_account_id'),
-    path('api/comments/update/<int:comNum>', CommentView.update_comment, name='update_comment'),
-    path('api/comments/delete/<int:comNum>', CommentView.delete_comment, name='delete_comment'),
-    path('api/medicines/', MedicineView.fetch_medicine, name='fetch_medicines'),
-    path('api/medicines/<str:drugID>', MedicineView.fetch_medicine_by_id, name='fetch_medicine'),
-    path('api/medicines/<str:presNum>', MedicineView.fetch_medicine_through_prescription, name='fetch_medicine_through_prescription'),
+    path('comments/create/<str:accountId>/<str:recordNum>/', CommentView.create_comment, name='create_comment'),
+    path('comments/get/', CommentView.fetch_comments, name='fetch_comments'),
+    path('comments/get/<str:accountId>', CommentView.fetch_comments_by_account_id, name='fetch_comments_by_account_id'),
+    path('comments/update/<int:comNum>', CommentView.update_comment, name='update_comment'),
+    path('comments/delete/<int:comNum>', CommentView.delete_comment, name='delete_comment'),
+    path('medicines/', MedicineView.fetch_medicine, name='fetch_medicines'),
+    path('medicines/<str:drugID>', MedicineView.fetch_medicine_by_id, name='fetch_medicine'),
+    path('medicines/<str:presNum>', MedicineView.fetch_medicine_through_prescription, name='fetch_medicine_through_prescription'),
 ]
