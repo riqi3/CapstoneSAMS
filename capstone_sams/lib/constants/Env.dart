094ffc186c48f7b0ae70a293c1c1910216a45517
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-08-04 12:56:26 +0800

    added additional ui on web

--- capstone_sams/lib/constants/Env.dart
+++ capstone_sams/lib/constants/Env.dart
@@ -3,6 +3,10 @@
 // }
 
 //ip from realme tablet
+// class Env {
+//   static const String prefix = "http://192.168.1.9:8000";
+// }
+
 class Env {
-  static const String prefix = "http://192.168.1.9:8000";
+  static const String prefix = "http://100.65.171.201:8000";
 }
