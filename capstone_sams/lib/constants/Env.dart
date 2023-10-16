4921bc75f141c835dfced5cb7a647eebe7e8f6bb
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-10-16 16:47:54 +0800

    update:test

--- capstone_sams/lib/constants/Env.dart
+++ capstone_sams/lib/constants/Env.dart
@@ -3,14 +3,14 @@
 // }
 
 // //ip from realme tablet
-// class Env {
-//   static const String prefix = "http://192.168.1.9:8000";
-// }
+class Env {
+  static const String prefix = "http://192.168.1.9:8000";
+}
 
 // class Env {
 //   static const String prefix = 'http://192.168.246.61:8000';
 // }
 
-class Env {
-  static const String prefix = 'http://192.168.235.61:8000';
-}
+// class Env {
+//   static const String prefix = 'http://192.168.235.61:8000';
+// }
--- capstone_sams/lib/sams_server/api/modules/patient/models.py
+++ capstone_sams/lib/sams_server/api/modules/patient/models.py
@@ -24,6 +24,9 @@ class Patient(models.Model):
     email = models.CharField(max_length = 50, blank = True)
     user = models.CharField(null = True, blank = True)
 
+    def __str__(self):
+        return f"{self.patientID} - {self.firstName} {self.middleName} {self.lastName}"
+
 '''
 This model represent the health records
 inputted and stored into the system. Each
