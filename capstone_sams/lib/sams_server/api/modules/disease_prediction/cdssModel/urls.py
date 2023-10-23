4ff977564b55e11dd4bc6f7a6f0da7eaffe334da
Justinxd <justin.gabison.20@usjr.edu.ph>
2023-10-23 22:05:30 +0800

    Implement upload dataset and retrain button

--- capstone_sams/lib/sams_server/api/modules/disease_prediction/cdssModel/urls.py
+++ capstone_sams/lib/sams_server/api/modules/disease_prediction/cdssModel/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cdss-dataset/', views.SymptomViewSet.as_view({'get': 'list'}), name='health-symptom-list'),
-    path('cdss-dataset/upload/', views.SymptomViewSet.as_view({'post': 'upload_data'}), name='upload-health-symptoms'),
     path('create_symptom_record/', views.create_symptom_record, name='create_symptom_record'),
     path('train-model/', views.TrainModelView.as_view(), name='train-model'),
     path('delete_symptom_record/<int:record_id>/', views.delete_symptom_record, name='delete_symptom_record'),
