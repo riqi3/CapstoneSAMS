1e905efd18091e7c7236d7a82768e2050a87056c
riqi3 <enrique.pacudan@usjr.edu.ph>
2024-01-03 03:18:09 +0800

    redesign patient info card

---
+++ capstone_sams/lib/global-widgets/texts/TitleValueText.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TitleValueText extends StatelessWidget {
+  const TitleValueText({
+    Key? key,
+    required this.title,
+    required this.value,
+  }) : super(key: key);
+
+  final String title, value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
+        Text(
+          value,
+        ),
+      ],
+    );
+  }
+}
