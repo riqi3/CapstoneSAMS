97d3293f51dd5e55bb575bdfcf1d01cadd6e2758
Mark Allen Talandron <markallen.talandron.20@usjr.edu.ph>
2024-01-05 20:53:00 +0800

    created url endpoints for present_illness

--- capstone_sams/lib/sams_server/api/modules/patient/urls.py
+++ capstone_sams/lib/sams_server/api/modules/patient/urls.py
@@ -1,4 +1,4 @@
-from .views import PatientView, MedicalRecordView,ContactPersonView
+from .views import PatientView, MedicalRecordView,ContactPersonView,PresentIllnessView
 from django.urls import path
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path('patients/history/<str:patientID>', MedicalRecordView.fetch_record_by_id, name='patient_record'), #API Endpoint for health record retrieval
     path('patients/history/update/<str:patientID>', MedicalRecordView.update_health_record, name='update_patient_record'),
     path('patients/contact/<str:patientID>', MedicalRecordView.fetch_contact_by_id, name='fetch_contact_by_id'),
-    path('patients/contact/update/<str:patientID>', ContactPersonView.update_contact_person, name='update_contact_person')
+    path('patients/contact/update/<str:patientID>', ContactPersonView.update_contact_person, name='update_contact_person'),
+    path('patients/complaint/<str:patientID>', PresentIllnessView.create_complaint, name='create_complaint'),
+    path('patients/complaint/update/<int:illnessNum>', PresentIllnessView.update_complaint, name='update_complaint'),
 ]
