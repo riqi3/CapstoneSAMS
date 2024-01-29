099be81a242d44a40c6324391d42d69a6e3d3698
riqi3 <enrique.pacudan@usjr.edu.ph>
2024-01-30 04:57:32 +0800

    minor fixes

--- capstone_sams/lib/screens/ehr-list/patient/order-entry/healthcheckscreen.dart
+++ capstone_sams/lib/screens/ehr-list/patient/order-entry/healthcheckscreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../../../providers/healthcheckprovider.dart';
+import '../../../../providers/HealthCheckProvider.dart';
 import 'widgets/buildwidgets.dart';
 
 class HealthCheckScreen extends StatelessWidget {
