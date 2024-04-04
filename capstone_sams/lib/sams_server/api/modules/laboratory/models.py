2ee6c94ef86e5d875fc6d425ff94bc44ae3e1a54
justinxdd <justin.gabison.20@usjr.edu.ph>
2024-04-05 03:21:13 +0800

    Soft deletion backend

--- capstone_sams/lib/sams_server/api/modules/laboratory/models.py
+++ capstone_sams/lib/sams_server/api/modules/laboratory/models.py
@@ -8,6 +8,7 @@ class LabResult(models.Model):
     pdf = models.FileField(blank = False,upload_to='upload-pdf/')
     patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
     user = models.CharField(null = True, blank = True)
+    isDeleted = models.BooleanField(default=False)
     def __str__(self):
         return self.title
     
